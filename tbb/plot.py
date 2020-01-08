3cb46e096ea3f9adc973c65bcd6b2209133321fa
Andrei Gogonea <andrei.gogonea@keysight.com>
2020-01-08 11:17:51 +0200

    Created first points image

--- tbb/plot.py
+++ tbb/plot.py
@@ -16,6 +16,23 @@ with open("initial_points") as f:
         y.append(int(y_elem))
         line = f.readline().strip()
 
+
+line_x = []
+line_y = []
+with open("final_points") as f:
+    line = f.readline().strip()
+    while line:
+        x_elem = line.split(" ")[0]
+        y_elem = line.split(" ")[1]
+        line_x.append(int(x_elem))
+        line_y.append(int(y_elem))
+        line = f.readline().strip()
+
+line_x.append(line_x[0])
+line_y.append(line_y[0])
 plt.plot(x, y, 'ro')
+plt.plot(line_x, line_y, 'ro-')
 plt.axis([0, 200, 0, 200])
 plt.show()
+
+
